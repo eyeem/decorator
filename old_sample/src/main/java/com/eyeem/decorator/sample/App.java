aee41bdd398a322805917891ccbc773694da893c
ronaldoeyeem <ronaldo@eyeem.com>
2016-07-28 10:49:57 +0200

    forgot to remove old sample

--- old_sample/src/main/java/com/eyeem/decorator/sample/App.java
+++
@@ -1,9 +0,0 @@
-package com.eyeem.decorator.sample;
-
-import com.eyeem.decorator.sample.mortar.MortarApplication;
-
-/**
- * Created by budius on 15.12.15.
- */
-public class App extends MortarApplication {
-}
