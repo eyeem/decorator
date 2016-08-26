22b7972880acabc37fdfdd2eb00f244b6404311e
Ronaldo Pace <ronaldo@eyeem.com>
2016-08-26 11:28:07 +0200

    update gradle, disable logs, abortOnErrorFalse

--- processor/src/main/java/com/eyeem/decorator/processor/Processor.java
+++ processor/src/main/java/com/eyeem/decorator/processor/Processor.java
@@ -23,9 +23,9 @@ public class Processor extends AbstractProcessor implements Log {
 
    private boolean logEnabled = true;
 
-   private static final boolean LOG_PARSER = true;
-   private static final boolean LOG_PARSER_RESULT = true;
-   private static final boolean LOG_GENERATION = true;
+   private static final boolean LOG_PARSER = false;
+   private static final boolean LOG_PARSER_RESULT = false;
+   private static final boolean LOG_GENERATION = false;
 
    @Override
    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
